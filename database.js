60d33615579bf03c3b303b1d121b60d0bed62abe
Sergey Stankevich <avaylon2009@gmail.com>
2019-04-16 15:43:38 +0300

    back inprogress

--- database.js
+++ database.js
@@ -2,23 +2,25 @@ module.exports = {
     selectCites: "select cites.id as id, cites.name as city, regions.name as region from cites left join regions ON (regions.id = cites.id_region)",
     selectAttrs: "select name, required from options",
     selectRegions: "select id, name from regions",
+    selectCity: (name) => (`select id from cites where name='${name}'`),
+    selectRegion: (name) => (`select id from regions where name='${name}'`),
     selectOptions: (id_city) => (`
         select options_values.id, options.name, options_values.value from options_values
         join cites ON (cites.id = options_values.id_city) 
         join options ON (options.id = options_values.id_option) 
         where id_city = ${id_city}
       `),
-    selectOptionID: (name) => `select id from options where name = ${name}`,
-    selectRegion: (name) => (`select id from regions where name='${name}'`),
+    selectOptionID: (name) => `select id from options where name = '${name}'`,
     deleteCity: (id) => (`delete from options_values where id_city = ${id}; delete from cites where id = ${id}`),
     deleteAttr: (id) => (`delete from options_values where id_option = ${id}; delete from options where id = ${id}`),
     changeAttr: (id, data) => (`update options_values set value = '${data.value}' where id = ${id}`),
     changeCity: (id, name, id_region) => (`update cites set name = '${name}', id_region = ${id_region} where id = ${id}`),
     createRegion: (name) => (`insert into regions(name) values('${name}')`),
     createOptionalOptions: (name) => (`
-        insert into options(name, required) values(${name}, false);
+        insert into options(name, required) values('${name}', false);
       `),
     createOptionalOptionsValue: (id_city, id_option, value) => (`
         insert into options_values(id_city, id_option, value) values(${id_city}, ${id_option}, '${value}')
       `),
+    createCity: (name, id_region) => (`insert into cites(name, id_region) values('${name}', ${id_region})`),
 };
--- server.js
+++ server.js
@@ -1,50 +1,11 @@
-
+const port = 3000;
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 const pgp = require("pg-promise")(/*options*/);
-const db_query = require('./database.js');
+const query = require('./database.js');
 
-const db = pgp({host: '82.146.58.11', user: 'postgres', port: '5432', database: 'new_db', password: '123'});
-
-const cites = [];
-const attrs = [];
-const attrsAll = [];
-const regions = [];
-
-const updateDBInfo = () => {
-    db.many(db_query.selectCites, null)
-        .then((data) => {
-            cites.splice(0, cites.length);
-            cites.push(...data)
-        })
-        .catch((error) => {
-            console.log("ERROR CITES:", error);
-        });
-
-
-    db.many(db_query.selectAttrs, null)
-        .then((data) => {
-            attrsAll.splice(0, attrsAll.length);
-            attrsAll.push(...data)
-        })
-        .catch((error) => {
-            console.log("ERROR ATTRS:", error);
-        });
-
-    db.many(db_query.selectRegions, null)
-        .then((data) => {
-            regions.splice(0, regions.length);
-            for (let elem of data) {
-                regions.push(elem.name)
-            }
-
-        })
-        .catch((error) => {
-            console.log("ERROR REGIONS:", error);
-        });
-};
-
+const database = pgp({host: '82.146.58.11', user: 'postgres', port: '5432', database: 'new_db', password: '123'});
 
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
@@ -54,133 +15,115 @@ app.use((req, res, next) => {
     next();
 });
 
-app.post('/auth', (req, res) => {
+const cites = [];
+const attrsAll = [];
+const regions = [];
 
-    if (req.body.login === 'admin' && req.body.password === 'merve') {
-        res.status(200).send(JSON.stringify({region: 'Россия', id: 30, token: 123, role: 'admin'}))
-    } else {
-        res.status(401).send('User not found')
-    }
+const DBind = (query, method) => (
+     database[method](query, null).catch((error) => (console.error(`Database Error: ${query} \n`, error)))
+);
 
+const updateDBInfo = async () => {
+    let data;
 
+    data = await DBind(query.selectCites, 'many');
+    cites.splice(0, cites.length);
+    cites.push(...data);
+
+    data = await DBind(query.selectAttrs, 'many');
+    attrsAll.splice(0, attrsAll.length);
+    attrsAll.push(...data);
+
+    data = await DBind(query.selectRegions, 'many');
+    regions.splice(0, regions.length);
+    for (let elem of data) {regions.push(elem.name)}
+};
+
+updateDBInfo();
+
+app.get('/regions', (req, res) => {
+    res.send(JSON.stringify(regions))
 });
-
-const changeCity = (id, name, id_region) => db.none(db_query.changeCity( id, name, id_region ), null)
-    .catch((error) => (console.error('change', error)));
-const createRegion = (name) => db.none(db_query.createRegion(name), null)
-    .catch((error) => (console.error('create', error)));
-const searchRegion = (region) => db.oneOrNone(db_query.selectRegion(region), null)
-    .catch((error) => (console.error('search', error)));
 
 app.put('/city/:uid', async (req, res) => {
     const {id, city, region} = req.body;
 
-    let regionInfo = await searchRegion(region);
+    let regionInfo = await DBind(query.selectRegion(region), 'oneOrNone');
     if (!regionInfo ) {
-        await createRegion(region);
-        regionInfo = await searchRegion(region);
+        await DBind(query.createRegion(name), 'none');
+        regionInfo = await DBind(query.selectRegion(region), 'oneOrNone');
     }
+    await DBind(query.changeCity(id,city,regionInfo.id), 'none');
 
-    await changeCity( id, city, regionInfo.id) ;
     updateDBInfo();
     res.send(JSON.stringify(req.body))
 });
 
+app.post('/city', async (req, res) => {
+    const {name, region} = req.body;
 
-app.put('/attributes/:uid', (req, res) => {
-    db.manyOrNone(db_query.changeAttr(req.params.uid, req.body), null)
-        .then(() => {
-            updateDBInfo();
-            res.send(JSON.stringify(req.body))
-        })
-        .catch((error) => {
-            console.log("ERROR UPDATE:", error);
-            res.status(404).end()
-        });
+    let regionInfo = await DBind(query.selectRegion(region), 'oneOrNone');
+    if (!regionInfo ) {
+        await DBind(query.createRegion(name), 'none');
+        regionInfo = await await DBind(query.selectRegion(region), 'oneOrNone');
+    }
+    await DBind(query.createCity(name,regionInfo.id), 'none');
+    const cityID = await DBind(query.selectCity(name), 'one');
+
+    updateDBInfo();
+    res.send(JSON.stringify( {id: cityID.id, city: name, region} ))
+});
+
+app.delete('/city/:uid', async (req, res) => {
+    await DBind(query.deleteCity(req.params.uid), 'none');
+
+    updateDBInfo();
+    res.send('OK')
+});
+
+
+app.get('/city/attributes/:uid/', async (req, res) => {
+    const data = await DBind(query.selectOptions(req.params.uid), 'manyOrNone');
+
+    res.send(JSON.stringify(data.map((currValue) => (
+        currValue.value === 'false' ? {...currValue, value: false} :
+            currValue.value === 'true' ? {...currValue, value: true} :
+                currValue
+    ))))
+});
+
+app.get('/city', (req, res) => {
+    res.send(JSON.stringify(cites))
 });
 
 app.get('/attributes/', (req, res) => {
     res.send(JSON.stringify(attrsAll))
 });
 
-app.delete('/attributes/:uid', (req, res) => {
-    db.none(db_query.deleteAttr(req.params.uid), null)
-        .then(() => {
-            updateDBInfo();
-            res.send('OK')
-        })
-        .catch((error) => {
-            console.log("ERROR DELETE:", error);
-            res.status(404).end()
-        });
+app.put('/attributes/:uid', async (req, res) => {
+    await DBind(query.changeAttr(req.params.uid, req.body), 'none');
+
+    updateDBInfo();
+    res.send(JSON.stringify(req.body))
 });
 
-app.delete('/city/:uid', (req, res) => {
-    db.none(db_query.deleteCity(req.params.uid), null)
-        .then( () => {
-            updateDBInfo();
-            res.send('OK');
-        })
-        .catch( (error) => {
-            console.log("ERROR DELETE:", error);
-            res.status(404).end()
-        });
-});
+app.delete('/attributes/:uid', async (req, res) => {
+    await DBind(query.deleteAttr(req.params.uid), 'none');
 
-
-app.get('/city/attributes/:uid/', (req, res) => {
-    db.many(db_query.selectOptions(req.params.uid), null)
-        .then( (data) => {
-            res.send(JSON.stringify(data.map((currValue) => (
-                currValue.value === 'false' ? {...currValue, value: false} :
-                    currValue.value === 'true' ? {...currValue, value: true} :
-                        currValue
-            ))))
-        })
-        .catch((error) => {
-            if (error.code === 0) {
-                res.status(404).end();
-            }
-        });
+    updateDBInfo();
+    res.send('OK')
 });
 
 app.post('/attributes', async (req, res) => {
     const {id_city, name, value} = req.body;
 
+    await DBind(query.createOptionalOptions(name), 'none');
+    const id_option = await DBind(query.selectOptionID(name), 'one');
+    await DBind(query.createOptionalOptionsValue(id_city, id_option.id, value), 'none');
 
-    await db.none(db_query.createOptionalOptions(name), null)
-        .catch( (error) => (console.error('createOptions', error)) );
-    const id_option = await db.none(db_query.selectOptionID(name), null)
-        .catch( (error) => (console.error('searchID', error)) );
-    await db.none(db_query.createOptionalOptionsValue(id_city, id_option, value), null)
-        .catch( (error) => (console.error('createOptions_value', error)) );
-
-    res.send(JSON.stringify({...req.body, id_option}))
+    res.send(JSON.stringify({id: id_option, name, value}))
 });
 
-var id_city = 1000
-app.post('/city', (req, res) => {
-
-
-    id_city++
-
-    var obj = {id: id_city, city: req.body.name, region: req.body.region}
-    cites.push(obj)
-
-    res.send(JSON.stringify(obj))
-});
-
-app.get('/regions', (req, res) => {
-
-
-    res.send(JSON.stringify(regions))
-});
-
-app.get('/city', (req, res) => {
-
-
-    res.send(JSON.stringify(cites))
-});
 app.post('/registr', (req, res) => {
     res.status(201).send(JSON.stringify({id: 40, token: 123, role: 'guest'}))
 });
@@ -195,7 +138,16 @@ app.post('/auth_token', (req, res) => {
 
 });
 
-app.listen(3000, () => {
-    updateDBInfo();
-    console.log('Done');
+app.post('/auth', (req, res) => {
+    if (req.body.login === 'admin' && req.body.password === 'merve') {
+        res.status(200).send(JSON.stringify({region: 'Россия', id: 30, token: 123, role: 'admin'}))
+    } else {
+        res.status(401).send('User not found')
+    }
+
+
+});
+
+app.listen(port, () => {
+    console.dir(`Server run on ${port} port`);
 });
